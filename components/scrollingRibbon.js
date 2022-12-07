ccdeb9ece56068a98968aad944976c841d6c962b
maybe vaggy <sarvagya6@gmail.com>
2022-12-07 14:12:59 -0500

    remove locomotive scroll

--- components/scrollingRibbon.js
+++ components/scrollingRibbon.js
@@ -1,28 +1,24 @@
-import { useLocomotiveScroll } from 'react-locomotive-scroll'
-
 export default function ScrollingRibbon() {
 
-    const { scroll } = useLocomotiveScroll()
-
     return (
-        <div data-scroll-container className="hidden lg:w-full h-[50%] lg:flex overflow-x-hidden font-fancy bg-white">
-            <div data-scroll-section className="hidden text-4xl lg:flex flex-row flex-nowrap justify-center py-6 items-center lg:absolute left-[-20%] bg-red-500 border-y-8 border-gray-800 rotate-12 z-10" >
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
+        <div className="hidden lg:w-full h-[50%] lg:flex overflow-x-hidden font-fancy bg-white">
+            <div className="hidden text-4xl lg:flex flex-row flex-nowrap justify-center py-6 items-center lg:absolute left-[-20%] bg-red-500 border-y-8 border-gray-800 rotate-12 z-10" >
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
             </div>
-            <div data-scroll-section className="hidden text-4xl lg:flex flex-row flex-nowrap justify-center py-6 items-center lg:absolute left-[-20%] bg-[#fbbf2d] border-y-8 border-gray-800 rotate-[-12deg] z-10" >
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
-                <span className="mx-4 whitespace-nowrap" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6">PROFHACKS</span>
+            <div className="hidden text-4xl lg:flex flex-row flex-nowrap justify-center py-6 items-center lg:absolute left-[-20%] bg-[#fbbf2d] border-y-8 border-gray-800 rotate-[-12deg] z-10" >
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
+                <span className="mx-4 whitespace-nowrap">PROFHACKS</span>
             </div>
         </div>
 
