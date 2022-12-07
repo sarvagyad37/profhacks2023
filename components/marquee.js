5ee898d35b484fe482c520413e83e53d676f9ba0
maybe vaggy <sarvagya6@gmail.com>
2022-12-07 13:35:22 -0500

    Update marquee.js

--- components/marquee.js
+++ components/marquee.js
@@ -3,22 +3,22 @@ import spin from "../public/assets/spin.svg"
 
 export default function Marquee() {
     return (
-        <div class="w-full flex overflow-x-hidden font-fancy bg-white border">
+        <div className="w-full flex overflow-x-hidden font-fancy bg-white border">
       
-        <div class="flex flex-row flex-nowrap justify-center items-center sm:py-12 animate-marquee2">
-          <span class="text-2xl sm:text-4xl mx-4 whitespace-nowrap">HAUNTED HACK</span>
-          <span class="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin"/></span>
-          <span class="text-2xl sm:text-4xl mx-4 whitespace-nowrap">RU READY?</span>
-          <span class="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin" /></span>
-          <span class="text-2xl sm:text-4xl mx-4 whitespace-nowrap">HAUNTED HACK</span>
-          <span class="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin" /></span>
-          <span class="text-2xl sm:text-4xl mx-4 whitespace-nowrap">RU READY?</span>
-          <span class="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin" /></span>
-          <span class="text-2xl sm:text-4xl mx-4 whitespace-nowrap">HAUNTED HACK</span>
-          <span class="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin" /></span>
-          <span class="text-2xl sm:text-4xl mx-4 whitespace-nowrap">RU READY?</span>
-          <span class="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin" /></span>
-        </div>
+        <div className="flex flex-row flex-nowrap justify-center items-center sm:py-12 animate-marquee2">
+          <span className="text-2xl sm:text-4xl mx-4 whitespace-nowrap">HAUNTED HACK</span>
+          <span className="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin"/></span>
+          <span className="text-2xl sm:text-4xl mx-4 whitespace-nowrap">RU READY?</span>
+          <span className="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin" /></span>
+          <span className="text-2xl sm:text-4xl mx-4 whitespace-nowrap">HAUNTED HACK</span>
+          <span className="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin" /></span>
+          <span className="text-2xl sm:text-4xl mx-4 whitespace-nowrap">RU READY?</span>
+          <span className="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin" /></span>
+          <span className="text-2xl sm:text-4xl mx-4 whitespace-nowrap">HAUNTED HACK</span>
+          <span className="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin" /></span>
+          <span className="text-2xl sm:text-4xl mx-4 whitespace-nowrap">RU READY?</span>
+          <span className="mx-4 whitespace-nowrap"><Image className="min-w-[80px] sm:min-w-[100px] animate-spin" src={spin} alt="spin" /></span>
+        </div>Name
       </div>
       
     )
