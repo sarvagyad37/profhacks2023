c5c953c6969c79b12335fb233b2dde1fb0012aae
maybe vaggy <sarvagya6@gmail.com>
2023-01-26 11:09:49 -0500

    added sponsors

--- components/sponsors.js
+++ components/sponsors.js
@@ -1,7 +1,12 @@
 import Image from "next/image";
 
 import WolframLanguageLogo from "../public/assets/sponsors/wolfram-language-logo.png"
-import NoStarchPressLogo from "../public/assets/sponsors/no-starch-press-logo.png"
+import RowanIEEELogo from "../public/assets/sponsors/rowan-ieee-logo.png"
+import NoStarchPressLogo from "../public/assets/sponsors/no-starch-press-logo.jpg"
+import IEEEPhillySectionLogo from "../public/assets/sponsors/ieee-philly-section-logo.jpg"
+import OreillyLogo from "../public/assets/sponsors/oreilly-logo.png"
+import RowanECELogo from "../public/assets/sponsors/rowan-ece-logo.png"
+import TaskadeLogo from "../public/assets/sponsors/taskade-logo.png"
 
 export default function Sponsors() {
     return (
@@ -9,7 +14,12 @@ export default function Sponsors() {
             <h1 className="text-4xl font-fancy text-center text-yellow-400 mt-[4rem] md:mt-[8rem]">Sponsors</h1>
             <div className="flex flex-col md:flex-row md:flex-wrap items-center justify-center w-full h-full">
                 <Image src={WolframLanguageLogo} alt="Wolfram Language Logo" className="h-[10rem] w-auto m-8"/>
+                <Image src={TaskadeLogo} alt="Taskade Logo" className="w-auto h-[10rem] m-8"/>
                 <Image src={NoStarchPressLogo} alt="No Starch Press Logo" className="w-auto h-[10rem] m-8"/>
+                <Image src={RowanIEEELogo} alt="Rowan IEEE Logo" className="w-auto h-[10rem] m-8"/>
+                <Image src={IEEEPhillySectionLogo} alt="IEEE Philly Section Logo" className="w-auto h-[7rem] m-8"/>
+                <Image src={OreillyLogo} alt="O'Reilly Logo" className="w-auto h-[6rem] m-8"/>
+                <Image src={RowanECELogo} alt="Rowan ECE Logo" className="w-auto h-[10rem] m-8"/>
             </div>
         </section>
     )
